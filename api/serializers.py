6291e300cffb5f22040ce590905a32e51f16736a
Pratik Raskar <raskarpratik44@gmail.com>
2026-05-25 14:25:00 +0530

    Remove all files from repository

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from society.models import Society
-from rest_framework import serializers
-
-class SocietySerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Society
-        fields="__all__"
-        
